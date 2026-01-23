aa61bf66eb152ce629e2dae84a7d3302beac82ed
Kenny Roger <202601132199@alunos.estacio.br>
2026-01-23 14:55:45 -0400

    Config: Enable DB emulate prepares for Supabase Pooler (Fix IPv6 issues)

--- config/database.php
+++ config/database.php
@@ -96,6 +96,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                \PDO::ATTR_EMULATE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
